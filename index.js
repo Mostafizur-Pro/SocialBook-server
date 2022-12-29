db3e26b018dad79054bf01c5f684b54bdb7bbfee
Mostafizur Rahman <mostafizur0195@gmail.com>
2022-12-29 11:27:02 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const userCollection = client.db("Socialbook").collection("UserInfomation");
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("SocilaBook Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`SocilaBook app listening on port ${port}`);
+});
