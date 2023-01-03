e7341ced8d6ef33e2b7208a6767aa1aa68c2dc1c
Mostafizur Rahman <mostafizur0195@gmail.com>
2023-01-03 20:05:17 +0600

    client site add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://Social-book:LIowRfchbgpfxzmo@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,6 +20,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("Socialbook").collection("UserInfomation");
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await userCollection.find(query).toArray();
+      res.send(users);
+    });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const users = await userCollection.findOne(query);
+      res.send(users);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
@@ -27,6 +41,7 @@ async function run() {
   } finally {
   }
 }
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("SocilaBook Hello World!");
